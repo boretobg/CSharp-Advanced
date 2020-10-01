db39ac587d02da3a83412877c53b5f58ae03d0b9
Bobby Stefanov <boretobg@gmail.com>
2020-10-01 20:20:02 +0300

    FolderSize Done

---
+++ StreamFilesAndDirectories-Lab/06.FolderSize/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace _06.FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles("../../../TestFolder"); //fileinfo
+            double sum = 0;
+            foreach (var item in files)
+            {
+                FileInfo info = new FileInfo(item);
+                sum += info.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+            File.WriteAllText("../../../output.txt", sum.ToString());
+        }
+    }
+}
