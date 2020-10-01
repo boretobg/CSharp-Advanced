44a5b43d9f161ca7845e4b76a5bc1f94da4b281a
Bobby Stefanov <boretobg@gmail.com>
2020-10-01 22:16:07 +0300

    LineNumbers Done

---
+++ StreamsFilesAndDirectories-Exercise/02.LineNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = File.ReadAllLines("../../../text.txt");
+            using var writer = new StreamWriter("../../../output.txt");
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int letters = 0;
+                int punct = 0;
+                for (int j = 0; j < text[i].Length; j++)
+                {
+                    if (text[i][j] != ' ')
+                    {
+                        if (text[i][j] == '-' || text[i][j] == ',' || 
+                            text[i][j] == '\'' || text[i][j] == '.')
+                        {
+                            punct++;
+                            continue;
+                        }
+                        letters++;
+                    }
+                }
+                writer.WriteLine($"Line {i + 1}: {text[i]}({letters})({punct})");
+
+            }
+        }
+    }
+}
