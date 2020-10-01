5bf684a2c4eb1346b0052db0f558b95ef9e8829c
Bobby Stefanov <boretobg@gmail.com>
2020-10-01 18:50:42 +0300

    SliceFile - Done

---
+++ StreamFilesAndDirectories-Lab/05.SliceFile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _05.SliceFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var slicer = new List<string>();
+            using (var reader = new StreamReader("../../../TargetFile.txt"))
+            {
+                var temp = string.Empty;
+                while (true)
+                {
+                    temp = reader.ReadLine();
+                    if (temp == null)
+                    {
+                        break;
+                    }
+                    slicer.Add(temp);
+                }
+            }
+
+            for (int i = 1; i <= slicer.Count; i++)
+            {
+                using (var writer = new StreamWriter($"../../../Sliced{i}.txt"))
+                {
+                    writer.WriteLine(slicer[i - 1]);
+                }
+            }
+        }
+    }
+}
