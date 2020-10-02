6cea7297b1d8af0786b3509e386f13cbf084693c
Bobby Stefanov <boretobg@gmail.com>
2020-10-02 14:04:00 +0300

    .

--- StreamsFilesAndDirectories-Exercise/04.CopyBinaryFile/Program.cs
+++ StreamsFilesAndDirectories-Exercise/04.CopyBinaryFile/Program.cs
@@ -8,6 +8,24 @@ namespace _04.CopyBinaryFile
     {
         static void Main(string[] args)
         {
+            using (var reader = new FileStream("../../../copyMe.png", FileMode.Open))
+            {
+                using (var writer = new FileStream("../../../final.png", FileMode.Create))
+                {
+                    byte[] buffer = new byte[4096];
+                    while (reader.CanRead)
+                    {
+                        int readBytes = reader.Read(buffer, 0, buffer.Length);
+
+                        if (readBytes == 0)
+                        {
+                            break;
+                        }
+                        writer.Write(buffer, 0, buffer.Length);
+                    }
+                }
+
+            }
         }
     }
 }
