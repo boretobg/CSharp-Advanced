21855c46672dd03948d982adc7348b3bfb54c686
Bobby Stefanov <boretobg@gmail.com>
2020-10-02 13:23:53 +0300

    ZipAndExtract Done

---
+++ StreamsFilesAndDirectories-Exercise/04.CopyBinaryFile/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _04.CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ StreamsFilesAndDirectories-Exercise/06.ZipAndExtract/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace _06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory("../../../copyme", "../../../zip.zip");
+            ZipFile.ExtractToDirectory("../../../zip.zip", "C:/Users/Bobby/Desktop");
+        }
+    }
+}
