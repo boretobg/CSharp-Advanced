79182cb523da0b5d5602a42cbababa6eae81c768
Bobby Stefanov <boretobg@gmail.com>
2020-10-06 19:17:58 +0300

    exercises

---
+++ FunctionProgramming-Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Func<string[], int, List<string>> editor = Predicator;
+            var list = editor(names, n);
+            Console.WriteLine(string.Join("\n", list));
+        }
+
+        static List<string> Predicator(string[] names, int n)
+        {
+            var output = new List<string>();
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i].Length <= n)
+                {
+                    output.Add(names[i]);
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ FunctionProgramming-Exercise/08.CustomComparator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Action<int[]> action = CustomSeparator;
+            action(nums);
+        }
+
+        static void CustomSeparator(int[] nums)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Predicate<int> isEven = IsEven;
+                if (isEven(nums[i]))
+                {
+                    list.Add(nums[i]);
+                }
+            }
+            list.Sort();
+
+            var list2 = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Predicate<int> isOdd = IsOdd;
+                if (isOdd(nums[i]))
+                {
+                    list2.Add(nums[i]);
+                }
+            }
+            list2.Sort();
+
+            Console.WriteLine($"{string.Join(" ", list)} {string.Join(" ", list2)}");
+        }
+
+        static bool IsEven(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        static bool IsOdd(int num)
+        {
+            if (num % 2 == 1 || num % 2 == -1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
