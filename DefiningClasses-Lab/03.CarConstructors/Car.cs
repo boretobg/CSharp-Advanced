65ac5f7d0df0b018269314768badda77dde7dfd4
Bobby Stefanov <boretobg@gmail.com>
2020-10-08 15:14:56 +0300

    Finished some exersices

---
+++ DefiningClasses-Lab/03.CarConstructors/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make = "VW";
+        public string Model = "Golf";
+        public int Year = 2025;
+        public double FuelQuantity = 200;
+        public double FuelConsumption = 10;
+
+        public Car(string make, string model, int year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public Car()
+        {
+
+        }
+    }
+}
