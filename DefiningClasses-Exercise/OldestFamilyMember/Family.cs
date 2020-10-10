65fe76d4a0fc4bdaed5ce7a3d8a99c34c8b2925a
Bobby Stefanov <boretobg@gmail.com>
2020-10-10 22:51:57 +0300

    .

---
+++ DefiningClasses-Exercise/OldestFamilyMember/Family.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace OldestFamilyMember
+{
+    public class Family
+    {
+        public List<Person> ListFamily { get; set; }
+
+        public Family(Person person)
+        {
+            this.ListFamily = person;
+        }
+
+        static void AddMember(Person person, List<Person> familyList)
+        {
+            familyList.Add(person);
+        }
+        static void GetOldestMember(Person person, List<Person> familyList)
+        {
+            familyList.Add(person);
+        }
+
+        public Family()
+        {
+
+        }
+    }
+}
