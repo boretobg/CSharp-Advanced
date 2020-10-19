c7c4727fba65191a9db8961b2058ac771e0878a3
Bobby Stefanov <boretobg@gmail.com>
2020-10-19 15:26:14 +0300

    FirstDone

---
+++ IteratorsAndComparators-Exercise/ListyIterator/ListyIterator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIteratorExercise
+{
+    public class ListyIterator<T>
+    {
+        public List<T> List { get; set; }
+        public int Count = 0;
+
+        public ListyIterator()
+        {
+            List = new List<T>();
+        }
+        public void Create(T[] elements)
+        {
+            for (int i = 1; i < elements.Length; i++)
+            {
+                List.Add(elements[i]);
+            }
+        }
+        public bool Move()
+        {
+            if (Count + 1 < List.Count)
+            {
+                Count++;
+                return true;
+            }
+            return false;
+        }
+        public bool HasNext()
+        {
+            if (Count + 1 < List.Count)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void Print()
+        {
+            if (List.Count == 0)
+            {
+                throw new System.ArgumentException("Invalid Operation!");
+            }
+            Console.WriteLine(List[Count]);
+        }
+    }
+}
---
+++ IteratorsAndComparators-Exercise/ListyIterator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ListyIteratorExercise
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> listyIterator = new ListyIterator<string>();
+
+            string[] create = Console.ReadLine().Split();
+            listyIterator.Create(create);
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                if (input == "Move")
+                {
+                    Console.WriteLine(listyIterator.Move());
+                }
+                else if (input == "HasNext")
+                {
+                    Console.WriteLine(listyIterator.HasNext());
+                }
+                else if (input == "Print")
+                {
+                    listyIterator.Print();
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
