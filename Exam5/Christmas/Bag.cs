b31b0625ad71d0ea18e21ea52d673c962c98264d
Bobby Stefanov <boretobg@gmail.com>
2020-10-24 21:12:55 +0300

    .

---
+++ Exam5/Christmas/Bag.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Data;
+
+namespace Christmas
+{
+    public class Bag
+    {
+        public List<Present> data;
+        public string Color { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return data.Count(); } }
+
+        public Bag(string color, int capacity)
+        {
+            this.Color = color;
+            this.Capacity = capacity;
+            this.data = new List<Present>();
+        }
+
+        public void Add(Present present)
+        {
+            if (Count < Capacity)
+            {
+                data.Add(present);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            var toRemove = data.FirstOrDefault(p => p.Name == name);
+            if (toRemove != null)
+            {
+                data.Remove(toRemove);
+                return true;
+            }
+            return false;
+        }
+
+        public Present GetHeaviestPresent()
+        {
+            var target = data.Max(p => p.Weight);
+            Present heaviest = data.FirstOrDefault(p => p.Weight == target);
+            return data.FirstOrDefault(p => p.Weight == target);
+        }
+
+        public Present GetPresent(string name)
+        {
+            return data.FirstOrDefault(p => p.Name == name);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} bag contains:");
+            foreach (var present in data)
+            {
+                sb.AppendLine(present.ToString());
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
